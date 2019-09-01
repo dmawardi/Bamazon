7393454e013087ed0ddff0b6cbed2db2208c4ce2
Dannar Mawardi <dmawardi@gmail.com>
2019-09-01 16:07:50 +1000

    Modified Schema to account for updates for challenge 3

--- schema.sql
+++ schema.sql
@@ -12,7 +12,16 @@ CREATE TABLE products (
   department_name VARCHAR(45) NULL,
   price DECIMAL(11,2) NOT NULL,
   stock_quantity INTEGER DEFAULT 0,
+  product_sales DECIMAL(11,2) DEFAULT 0,
   PRIMARY KEY (item_id)
+);
+
+-- Create departments table
+CREATE TABLE departments (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(45) NOT NULL,
+  over_head_costs DECIMAL(11,2) NOT NULL,
+  PRIMARY KEY (department_id)
 );
 
 -- Insert mock products 
