921e888d7386dadad821ad3d91d49558f24abefa
Dannar Mawardi <dmawardi@gmail.com>
2019-09-01 20:57:04 +1000

    Completed create new dept fn and vew to see new depts

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -23,40 +23,31 @@ var data;
 // Prompt user for additional answers given they select to add a new product
 function promptForDepartmentAdd() {
     return inquirer
-        .prompt([{
-                name: 'product_name',
-                type: 'input',
-                message: "What's the name of the product you wish to add?"
-
-            },
+        .prompt([
             {
                 name: 'department_name',
                 type: 'input',
                 message: "What's the dept/category of the item?"
             },
             {
-                name: 'price',
+                name: 'over_head_costs',
                 type: 'input',
-                message: "What's the retail price of the item?"
-            },
-            {
-                name: 'stock_quantity',
-                type: 'input',
-                message: "Is there an initial quantity stock for this item?"
+                message: "What are the overhead costs of the department?"
             }
+            
         ])
         .then(answers => {
-            return Promise.resolve(addNewProduct(answers.product_name, answers.department_name, answers.price, answers.stock_quantity));
+            addNewDepartment(answers.department_name,answers.over_head_costs);
 
         });
 }
 
 function fetchSalesByDept() {
     console.log("Fetching product sales by Department...\n");
-    let query = ['SELECT b.department_id, a.department_name, a.product_sales,b.over_head_costs',
-    'FROM BamazonDB.products as a INNER JOIN BamazonDB.departments as b',
+    let query = ['SELECT b.department_id, b.department_name, a.product_sales,b.over_head_costs',
+    'FROM BamazonDB.products as a RIGHT OUTER JOIN  BamazonDB.departments as b',
     'on a.department_name=b.department_name',
-    'GROUP BY a.department_name, b.department_id, a.product_sales, b.over_head_costs'
+    'GROUP BY b.department_id, a.department_name, a.product_sales, b.over_head_costs'
     ].join(' ')
     connection.query(query, function (err, res) {
         if (err) throw err;
@@ -68,28 +59,28 @@ function fetchSalesByDept() {
 }
 
 // Adds to current inventory level if addQuantity is greater than 0
-function addNewDepartment(product_name, department_name, price, stock_quantity) {
-    console.log(product_name, department_name, price, stock_quantity);
+function addNewDepartment(department_name, over_head_costs) {
+    console.log(department_name, over_head_costs);
 
-    if (product_name && department_name && price) {
+    if (over_head_costs && department_name) {
         // Insert a new item into the products table
-        let query = 'INSERT INTO BamazonDB.products (product_name, department_name, price, stock_quantity) VALUES (?,?,?,?)';
+        let query = 'INSERT INTO BamazonDB.departments (department_name, over_head_costs) VALUES (?,?)';
         let sql = connection.query(query,
-            [product_name, department_name, price, stock_quantity],
+            [department_name, parseInt(over_head_costs)],
             // callback function
             function (err, res) {
                 if (err) throw err;
-                console.log(sql);;
+                console.log(sql);
                 // Alert user new product added
-                console.log('New Product Added!\n');
-                // Send user back to admin menu
+                console.log('New Department Added!\n');
+                // Send user back to supervisor menu
                 supervisorMenu();
             });
     } else {
         // Provide user prompt as to why the product failed to be added
-        console.log('You must at least provide a product name, department name and price!');
+        console.log('Failed to add department!\nYou must provide a department name and the overhead costs!');
 
-        adminMenu();
+        supervisorMenu();
     }
 
 
@@ -110,6 +101,7 @@ function supervisorMenu() {
                     fetchSalesByDept();
                     break;
                 case 'Create New Department':
+                    promptForDepartmentAdd();
                     break;
                
                 case 'Exit':
